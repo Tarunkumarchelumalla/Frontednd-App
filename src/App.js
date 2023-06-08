2ed1397d12f19026579ae2c4c6a9656fc6386527
Tarunkumarchelumalla <tarunchelumalla@gmail.com>
2023-06-08 22:15:46 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-
+import Navbaar from './NavbarComponent';
+//  purple #6441F0 #rgb(67,66,69)
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Navbaar></Navbaar>
   );
 }
 
