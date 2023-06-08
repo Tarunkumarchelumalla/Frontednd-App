1c4c3661109760e527401a5b74d731077664bdf5
Tarunkumarchelumalla <tarunchelumalla@gmail.com>
2023-06-09 00:01:57 +0530

    second one

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import logo from './logo.svg';
 import './App.css';
 import Navbaar from './NavbarComponent';
+import SecondComponent from './secondComponent';
 //  purple #6441F0 #rgb(67,66,69)
 function App() {
   return (
-    <Navbaar></Navbaar>
+    <><div className='h-[100vh] w-[100%]  my-10 mx-10'>
+        <Navbaar></Navbaar>
+        <SecondComponent></SecondComponent>
+     </div>
+    </>
   );
 }
 
---
+++ src/secondComponent.js
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function SecondComponent() {
+  return (
+    <div className='flex flex-row gap-4 mx-auto max-w-7xl  sm:px-6 lg:px-8 '>
+        <div className='flex w-[30%] flex-row items-center'>
+            <p className='text-3xl text-black'> EQ is Better Than IQ</p>
+        </div>
+        <div className='flex w-[30%] flex-row items-center'>
+          
+        </div>
+        <div className='flex w-[30%] flex-row items-center'>
+          
+        </div>
+    </div>
+  )
+}
+
+export default SecondComponent
